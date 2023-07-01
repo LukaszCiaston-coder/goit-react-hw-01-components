9126a16c8399cec8cc5ce2c5e0edd0169324ac80
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-07-01 21:20:07 +0200

    friendList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,32 +2,42 @@ import React, { useState } from "react";
 import './App.css';
 import { Profile } from "./Profile/Profile";
 import { Statistic } from "./Statistic/Statistic";
+import { FriendList } from "./FriendList/FriendList";
 
 export const App = () => {
   const [showProfile, setShowProfile] = useState(false);
   const [showStatistic, setShowStatistic] = useState(false);
+  const [showFriendList, setShowFriendList]= useState(false)
 
   const handleShowProfile = () => {
     setShowProfile(true);
     setShowStatistic(false);
+    setShowFriendList(false);
   };
 
   const handleShowStatistic = () => {
     setShowProfile(false);
     setShowStatistic(true);
+    setShowFriendList(false);
   };
-
+  const handleShowFriendList = () => {
+    setShowProfile(false);
+    setShowStatistic(false);
+    setShowFriendList(true);
+  };
   const handleBack = () => {
     setShowProfile(false);
     setShowStatistic(false);
+    setShowFriendList(false);
   };
 
   return (
     <>
-      {!showProfile && !showStatistic ? (
+      {!showProfile && !showStatistic && !showFriendList? (
         <>
           <button onClick={handleShowProfile}>Show Profile</button>
           <button onClick={handleShowStatistic}>Show Statistic</button>
+          <button onClick={handleShowFriendList}>Show Friend</button>
         </>
       ) : (
         <>
@@ -42,7 +52,13 @@ export const App = () => {
               <Statistic />
               <button onClick={handleBack}>Back</button>
             </>
-          )}
+            )}
+            {showFriendList && (
+              <>
+                <FriendList />
+               <button onClick={handleBack}>Back</button>
+            </> 
+            )}
         </>
       )}
     </>
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,1 +1,24 @@
-import PropTypes from 'prop-types';
+
+import css from '../FriendList/FriendList.module.css';
+import friends from '../FriendList/friends.json';
+
+export const FriendList = () => {
+  return (
+    <ul className={css['friend-list']}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        const statusClassName = isOnline ? css.online : css.offline;
+        return (
+          <li className={css.item} key={id}>
+            <span className={`${css.status} ${statusClassName}`}></span>
+            <img src={avatar} alt={name} className={css.avatar} />
+            <p className={css.name}>{name}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+
+
+
