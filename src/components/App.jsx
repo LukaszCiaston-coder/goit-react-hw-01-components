5015178b757dceb5c2635548cbaebbe959a0b0e4
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-07-01 19:23:50 +0200

    statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,51 @@
-
+import React, { useState } from "react";
+import './App.css';
 import { Profile } from "./Profile/Profile";
+import { Statistic } from "./Statistic/Statistic";
+
 export const App = () => {
-    return (
-      <>
-        <Profile/>
-      
-      </>
-    )
-  };  
+  const [showProfile, setShowProfile] = useState(false);
+  const [showStatistic, setShowStatistic] = useState(false);
+
+  const handleShowProfile = () => {
+    setShowProfile(true);
+    setShowStatistic(false);
+  };
+
+  const handleShowStatistic = () => {
+    setShowProfile(false);
+    setShowStatistic(true);
+  };
+
+  const handleBack = () => {
+    setShowProfile(false);
+    setShowStatistic(false);
+  };
+
+  return (
+    <>
+      {!showProfile && !showStatistic ? (
+        <>
+          <button onClick={handleShowProfile}>Show Profile</button>
+          <button onClick={handleShowStatistic}>Show Statistic</button>
+        </>
+      ) : (
+        <>
+          {showProfile && (
+            <>
+              <Profile />
+              <button onClick={handleBack}>Back</button>
+            </>
+          )}
+          {showStatistic && (
+            <>
+              <Statistic />
+              <button onClick={handleBack}>Back</button>
+            </>
+          )}
+        </>
+      )}
+    </>
+  );
+};
 
--- src/components/Statistic/Statistic.jsx
+++ src/components/Statistic/Statistic.jsx
@@ -1,1 +1,22 @@
-import PropTypes from 'prop-types';
+import css from '../Statistic/Statistic.module.css';
+import data from '../Statistic/data.json';
+
+export const Statistic = () => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>Statistics</h2>
+      <ul className={css['stat-list']}>
+        {data.map(({ label, percentage, id }) => { 
+          return (
+            <li className={css.item} key={id}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistic;
